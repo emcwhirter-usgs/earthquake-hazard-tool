dd3bcfaaca82817cace17e35a1cb57cac349c609
Eddie McWhirter <emcwhirter@usgs.gov>
2015-11-10 12:47:41 -0700

    Add DeaggregationGraphView to browserify.

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -58,6 +58,7 @@ addExports(config.src + '/htdocs/js', [
   'Calculator',
   'ContourTypeView',
   'DataExport',
+  'DeaggregationGraphView',
   'DependencyFactory',
   'DynamicHazardResponse',
   'EditionView',

25cab45b311a0efb6df818f6968e551ce61e7175
Eddie McWhirter <emcwhirter@usgs.gov>
2015-11-10 12:47:12 -0700

    Create DeaggregationGraphView skeleton.

---
+++ src/htdocs/js/DeaggregationGraphView.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+var D3View = require('d3/D3View'),
+    Util = require('util/Util');
+
+
+var DeaggregationGraphView = function (options) {
+  var _this,
+      _initialize,
+
+      _title;
+
+  _this = D3View(Util.extend({
+    xLabel: 'Closest Distance, Rcd (km)',
+    yLabel: 'MAGNITUDE (Mw)',
+    zLabel: '% Contribution to Hazard'
+  }, options));
+
+
+  _initialize = function (options) {
+    _this.el.classList.add('DeaggregationGraphView');
+
+    _title = options.title;
+  };
+
+
+  /**
+   * Unbind event listeners and free references.
+   */
+  _this.destroy = Util.compose(function () {
+    _title = null;
+
+  }, _this.destroy);
+
+  _this.render = Util.compose(function (changed) {
+
+    // pass argument to original render method.
+    return changed;
+  }, _this.render);
+
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+
+module.exports = DeaggregationGraphView;

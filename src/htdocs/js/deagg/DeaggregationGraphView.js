17a3bb50fea6a2c092fce803003f1a8a15d830e9
Eddie McWhirter <emcwhirter@usgs.gov>
2015-11-12 12:37:43 -0700

    Move DeaggregationGraphView source into deagg sub-directory.

--- src/htdocs/js/deagg/DeaggregationGraphView.js
+++ src/htdocs/js/deagg/DeaggregationGraphView.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SelectedCollectionView = require('mvc/SelectedCollectionView'),
+
     Util = require('util/Util');
 
 
@@ -16,9 +17,18 @@ var DeaggregationGraphView = function (options) {
 
   _initialize = function (options) {
     options = Util.extend({}, _DEFAULTS, options);
+
+    _this.el.classList.add('DeaggregationGraphView');
   };
 
 
+  /**
+   * Unbind event listeners and free references.
+   */
+  _this.destroy = Util.compose(function () {
+
+  }, _this.destroy);
+  
   _this.render = function () {
     _this.el.innerHTML = '<pre>' +
         JSON.stringify(_this.model, null, '  ') +

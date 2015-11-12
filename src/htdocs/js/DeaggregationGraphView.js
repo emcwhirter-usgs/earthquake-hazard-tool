1bce372d82c3cd8bab0a592514b684de97f299ce
Eddie McWhirter <emcwhirter@usgs.gov>
2015-11-12 12:22:45 -0700

    Clean out parts of DeaggregationGraphView that are handled in DeaggResponse.

--- src/htdocs/js/DeaggregationGraphView.js
+++ src/htdocs/js/DeaggregationGraphView.js
@@ -1,37 +1,24 @@
 'use strict';
 
 
-var D3View = require('d3/D3View'),
+// var D3View = require('d3/D3View'),
+
+var SelectedCollectionView = require('mvc/SelectedCollectionView'),
+
     Util = require('util/Util');
 
 
 var DeaggregationGraphView = function (options) {
   var _this,
-      _initialize,
+      _initialize;
 
-      _deaggregations,
-      _imt,
-      _mlabel,
-      _rlabel,
-      _εbins,
-      _εlabel;
-
-  _this = D3View(Util.extend({
-    rLabel: 'Closest Distance, Rcd (km)',
-    mLabel: 'MAGNITUDE (Mw)',
-    εLabel: '% Contribution to Hazard'
-  }, options));
+  _this = SelectedCollectionView(Util.extend({}, options));
 
 
-  _initialize = function (options) {
+  _initialize = function (/*options*/) {
     _this.el.classList.add('DeaggregationGraphView');
 
-    _deaggregations = options.deaggregations;
-    _imt = options.imt;
-    _mlabel = options.mlabel;
-    _rlabel = options.rlabel;
-    _εbins = options.εbins;
-    _εlabel = options.εlabel;
+    console.log(_this);
   };
 
 
@@ -39,16 +26,11 @@ var DeaggregationGraphView = function (options) {
    * Unbind event listeners and free references.
    */
   _this.destroy = Util.compose(function () {
-    _deaggregations = null;
-    _imt = null;
-    _mlabel = null;
-    _rlabel = null;
-    _εbins = null;
-    _εlabel = null;
 
   }, _this.destroy);
 
   _this.render = Util.compose(function (changed) {
+    console.log(_this.model);
 
     // pass argument to original render method.
     return changed;
